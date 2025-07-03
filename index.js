f806826a1caa1ca1e28f23fc1d4290ace8b3d0ab
KlaiGhassen <ghassen.klai@esprit.tn>
2025-07-03 21:14:46 +0100

    sockot io

--- index.js
+++ index.js
@@ -4,20 +4,48 @@ const http = require("http");
 const morgan = require("morgan");
 const configDb = require("./dbConfig/configMongo.json");
 const chatRoutes = require("./routes/ChatRoute");
+const path = require("path");
+const { addchatScocket } = require("./controllers/ChatController");
 
 const app = express();
-app.use(morgan("dev"));
-
+ app.use(morgan("dev"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "twig");
 mongoose.connect(configDb.uri).then(() => {
   console.log("Connected to MongoDB");
 });
 app.use(express.json());
-app.use("/api/chats", chatRoutes);
+app.use("/chats", chatRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Notify the user who just connected
+  socket.emit("systemMessage", "You are connected");
+
+  // Listen for a new chat message from a user
+  socket.on("newChatMessage", (messageData) => {
+    addchatScocket(messageData); // Store message in DB or memory
+    io.emit("newChatMessage", messageData); // Send to all users
+  });
+
+  // Listen for "user is typing" events
+  socket.on("userTyping", (typingData) => {
+    socket.broadcast.emit("userTyping", typingData); // Notify others
+  });
+
+  // Handle user disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+    io.emit("systemMessage", "A user has disconnected");
+  });
 });
 
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
 
 // error handler
 app.use(function (err, req, res, next) {
--- routes/ChatRoute.js
+++ routes/ChatRoute.js
@@ -3,8 +3,11 @@ const express = require("express");
 const router = express.Router();
 const validateChat = require("../middeleware/valide");
 
-router.get("/", ChatController.getChats);
-router.post("/",  ChatController.addChat, (req, res) => {
+router.get("/", (req, res) => {
+  res.render("chat");
+});
+
+router.post("/", ChatController.addChat, (req, res) => {
   console.log("Chat added successfully");
   res.status(201).json({ message: "Chat added successfully" });
 });
